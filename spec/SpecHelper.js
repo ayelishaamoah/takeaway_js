9d8712472485113eb688c69109bf5144044ff225
Student <student@Makerss-MacBook-Pro.local>
2019-07-04 15:54:28 +0100

    Restructured files and removed boilerplate

--- spec/SpecHelper.js
+++ spec/SpecHelper.js
@@ -1,15 +1,16 @@
-beforeEach(function () {
-  jasmine.addMatchers({
-    toBePlaying: function () {
-      return {
-        compare: function (actual, expected) {
-          var player = actual;
-
-          return {
-            pass: player.currentlyPlayingSong === expected && player.isPlaying
-          };
-        }
-      };
-    }
-  });
-});
+// You can add your own custom matchers
+// beforeEach(function () {
+//   jasmine.addMatchers({
+//     toBePlaying: function () {
+//       return {
+//         compare: function (actual, expected) {
+//           var player = actual;
+//
+//           return {
+//             pass: player.currentlyPlayingSong === expected && player.isPlaying
+//           };
+//         }
+//       };
+//     }
+//   });
+// });
