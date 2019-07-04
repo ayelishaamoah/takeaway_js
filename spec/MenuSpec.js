611c9f94070a118cf8ef80662ce0521cdf7ebdad
Student <student@Makerss-MacBook-Pro.local>
2019-07-04 16:55:43 +0100

    Can add items to an order

--- spec/MenuSpec.js
+++ spec/MenuSpec.js
@@ -1,6 +1,11 @@
 // As a customer
 // So that I can check if I want to order something
 // I would like to see a list of dishes with prices
+//
+// As a customer
+// So that I can order the meal I want
+// I would like to be able to select some number of several available dishes
+
 
 describe('Menu', function(){
   var menu = new Menu();
@@ -8,5 +13,9 @@ describe('Menu', function(){
   it('displays a list of dishes with prices', function() {
     menuItems = menu.menuItems;
     expect(menu.display()).toEqual(menuItems);
+  })
+
+  it('can select items from the menu', function() {
+    expect(menu.selectItem('Roast Chicken', 2)).toContain(['Roast Chicken', 2])
   })
 });
--- src/Menu.js
+++ src/Menu.js
@@ -2,10 +2,16 @@ function Menu(){
   this.menuItems = {
     'Roast Chicken': 13.95,
     'Roast Beef': 14.95,
-    'Vegetarian Roast': 11.95 
-  }
+    'Vegetarian Roast': 11.95
+  };
+  this.order = [];
 }
 
 Menu.prototype.display = function () {
   return this.menuItems
+};
+
+Menu.prototype.selectItem = function (item, quantity) {
+  this.order.push([item, quantity])
+  return this.order
 };
