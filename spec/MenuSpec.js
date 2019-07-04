838c11a733e3301d9720385e31c0c3ba5ee3af86
Student <student@Makerss-MacBook-Pro.local>
2019-07-04 18:08:14 +0100

    Can see the order total

--- spec/MenuSpec.js
+++ spec/MenuSpec.js
@@ -1,21 +1,34 @@
 // As a customer
 // So that I can check if I want to order something
 // I would like to see a list of dishes with prices
-//
+
 // As a customer
 // So that I can order the meal I want
 // I would like to be able to select some number of several available dishes
 
+// As a customer
+// So that I can verify that my order is correct
+// I would like to check that the total I have been given matches the sum of the various dishes in my order
 
 describe('Menu', function(){
-  var menu = new Menu();
+  var menu;
+
+  beforeEach(function() {
+    menu = new Menu();
+  });
 
   it('displays a list of dishes with prices', function() {
     menuItems = menu.menuItems;
     expect(menu.display()).toEqual(menuItems);
-  })
+  });
 
   it('can select items from the menu', function() {
-    expect(menu.selectItem('Roast Chicken', 2)).toContain(['Roast Chicken', 2])
-  })
+    expect(menu.selectItem('Roast Chicken', 2)).toEqual({'Roast Chicken': 2});
+  });
+
+  it('displays the total', function() {
+    menu.selectItem('Roast Chicken', 2);
+    menu.selectItem('Roast Beef', 1);
+    expect(menu.getTotal()).toEqual('42.85')
+  });
 });
--- src/Menu.js
+++ src/Menu.js
@@ -4,7 +4,7 @@ function Menu(){
     'Roast Beef': 14.95,
     'Vegetarian Roast': 11.95
   };
-  this.order = [];
+  this.order = {};
 }
 
 Menu.prototype.display = function () {
@@ -12,6 +12,16 @@ Menu.prototype.display = function () {
 };
 
 Menu.prototype.selectItem = function (item, quantity) {
-  this.order.push([item, quantity])
+  this.order[item] = quantity
   return this.order
+};
+
+Menu.prototype.getTotal = function () {
+  var orderTotal = 0;
+  for (var item in this.order ) {
+    price = this.menuItems[item];
+    quantity = this.order[item]
+    orderTotal += (price * quantity)
+  }
+  return orderTotal.toFixed(2);
 };
